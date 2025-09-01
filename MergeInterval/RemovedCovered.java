4295b5df4a6f5d5c9cda95ae3db372cdbccdbfab
Dev Tayal <devtayal04@gmail.com>
2025-09-01 23:51:01 +0530

    covered remove

---
+++ MergeInterval/RemovedCovered.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+         Arrays.sort(intervals, (a,b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        int count = 0;
+        int preStart = intervals[0][0];
+        int prevEnd = intervals[0][1];
+        
+        for(int i=1; i <intervals.length ; i++){
+            if(intervals[i][0] >= preStart && intervals[i][1] <= prevEnd ){
+                count++;
+            } else{
+                preStart = intervals[i][0];
+                prevEnd = intervals[i][1];
+            }
+        }
+      return intervals.length - count;
+    }
+}
